ce389d783e0f045986770055c5cda099678adb24
Rakesh Biswas <rbiswas01999@gmail.com>
2025-02-15 13:50:25 +0600

    dynamic route modified

--- src/app/products/[productId]/page.js
+++ src/app/products/[productId]/page.js
@@ -1,10 +1,13 @@
-
-const DynamicProductPage = () => {
-    return (
-        <div>
-            <h1 className="text-xl text-center">This is Dynamic Product Page </h1>
-        </div>
-    );
+const DynamicProductPage = async ({ params, searchParams }) => {
+  const productId = await params.productId;
+  console.log(await searchParams);
+  return (
+    <div>
+      <h1 className="text-xl text-center">
+        This is Dynamic Product {productId}{" "}
+      </h1>
+    </div>
+  );
 };
 
 export default DynamicProductPage;
