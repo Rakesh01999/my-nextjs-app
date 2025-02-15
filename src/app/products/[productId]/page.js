c8aa12dfc5cb6f755618256c3d6777c6306c0f1e
Rakesh Biswas <rbiswas01999@gmail.com>
2025-02-15 13:23:50 +0600

    dynamci route created

---
+++ src/app/products/[productId]/page.js
@@ -0,0 +1,10 @@
+
+const DynamicProductPage = () => {
+    return (
+        <div>
+            <h1 className="text-xl text-center">This is Dynamic Product Page </h1>
+        </div>
+    );
+};
+
+export default DynamicProductPage;
