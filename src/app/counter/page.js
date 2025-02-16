b9f2b886e293c28e17d6f671389abb64dc785059
Rakesh Biswas <rbiswas01999@gmail.com>
2025-02-16 15:51:19 +0600

    components folder created

--- src/app/counter/page.js
+++ src/app/counter/page.js
@@ -1,34 +1,12 @@
-"use client";
+import Counter from "@/components/Counter";
 
-import { useState } from "react";
-
-const Counter = () => {
-  const [count, setCount] = useState(0);
+const CounterPage = () => {
   console.log("hello from counter");
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <div className="bg-white shadow-md rounded-lg p-6 w-80">
-        <h1 className="text-2xl font-bold text-center mb-4 text-gray-800">
-          Simple Counter
-        </h1>
-        <div className="flex items-center justify-center space-x-4">
-          <button
-            onClick={() => setCount(count - 1)}
-            className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-800"
-          >
-            -
-          </button>
-          <span className="text-3xl font-semibold text-gray-800">{count}</span>
-          <button
-            onClick={() => setCount(count + 1)}
-            className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600"
-          >
-            +
-          </button>
-        </div>
-      </div>
+    <div>
+      <Counter />
     </div>
   );
 };
 
-export default Counter;
+export default CounterPage;
