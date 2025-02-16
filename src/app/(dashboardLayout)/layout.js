82245117a58346e0b7ca7d9201c09a38f677f77e
Rakesh Biswas <rbiswas01999@gmail.com>
2025-02-16 17:02:41 +0600

    (dashboardLayout) -layout added

---
+++ src/app/(dashboardLayout)/layout.js
@@ -0,0 +1,11 @@
+const DashoboardLayout = ({ children }) => {
+  return (
+    <div>
+      <header>Navbar</header>
+      {children}
+      <footer>Footer</footer>
+    </div>
+  );
+};
+
+export default DashoboardLayout;
