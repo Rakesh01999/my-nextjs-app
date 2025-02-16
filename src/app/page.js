d75a2238b3b17df4cf3247ede81c942f34c27928
Rakesh Biswas <rbiswas01999@gmail.com>
2025-02-16 16:49:46 +0600

    dashboard routing handle differently

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
+"use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const HomePage = () => {
+  const router = useRouter();
+  const handleNavigation = () => {
+    router.push("/dashboard");
+  };
   return (
     <div>
       <h1 className="text-3xl text-center">Welcome to Home page</h1>
-      <Link href="/dashboard">
-        <button>Dashboard</button>
-      </Link>
+      <button onClick={handleNavigation} className="my-5">Dashboard</button>
     </div>
   );
 };
